7e61df6502f6e299150ce07f0c76a317bae15e08
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-12-08 08:45:34 +0800

    added basic home page setup

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    context = {
+    }
+    return render(request, 'home.html', context)
