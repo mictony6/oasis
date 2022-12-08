09154749deae24e4cd7325f1509b91ea763c2078
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-12-08 13:48:11 +0800

    updated database to match database schema

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,43 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.urls import reverse
+
+
+class ForumUser(User):
+    class Meta:
+        verbose_name = "forumuser"
+        verbose_name_plural = "forumusers"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("forumuser_detail", kwargs={"pk": self.pk})
+
+
+class Post(models.Model):
+    categoryID = models.IntegerField(blank=False)
+    userID = models.ForeignKey(ForumUser, on_delete=models.CASCADE)
+    parentPostID = models.IntegerField(blank=True, null=True)
+    title = models.CharField(max_length=90)
+    content = models.TextField(blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(ForumUser, related_name="likes", blank=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Post_detail", kwargs={"pk": self.pk})
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
