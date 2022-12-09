ad704cebc6b6f1b2feb794f026aef8f903b40d19
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-12-09 09:27:32 +0800

    hover effects for side bar

--- forum/static/js/home.js
+++ forum/static/js/home.js
@@ -1,5 +1,4 @@
 let contactButton = document.getElementById("contact-button")
-let newPostTrigger = document.getElementById("new-post-area")
 
 contactButton.onclick = function (){
     contactButton.style = "background-color:#408643FF"
