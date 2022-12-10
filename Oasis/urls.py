97aa537ededac0cd05e83168d4733f9fe2eda675
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2022-12-10 22:02:09 +0800

    minor changes

--- Oasis/urls.py
+++ Oasis/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 # from django.conf import settings
 # from django.conf.urls.static import static
@@ -24,4 +26,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forum.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/logo2.png'))),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
